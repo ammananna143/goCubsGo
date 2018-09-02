ad70c14e94030f265993dbaacf33092bdfd0b3c6
ammananna143 <ram.guru064@gmail.com>
2018-09-02 23:36:21 +0530

    add files

---
+++ src/test/java/com/jruffin/app/AppTest.java
@@ -0,0 +1,22 @@
+package com.jruffin.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import static java.lang.System.out;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+	out.println("welcome to Dev + Ops");    
+        assertTrue( true );
+    }
+}
